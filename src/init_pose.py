58e1d0a306a4a604c3d76c9b1938f657c7b56c06
irgups <romankovsa@yandex.ru>
2020-10-27 20:20:00 +0800

    reassembly

--- src/init_pose.py
+++ src/init_pose.py
@@ -15,19 +15,19 @@ def time_wait():
 
 	while not rospy.is_shutdown():
 		if key == 1:
-		pose = PoseWithCovarianceStamped()
-		pose.header.frame_id = "map"
-		pose.header.stamp = rospy.Time.now()
+			pose = PoseWithCovarianceStamped()
+			pose.header.frame_id = "map"
+			pose.header.stamp = rospy.Time.now()
 
-		pose.pose.pose.position.x = 17.5
-		pose.pose.pose.position.y = 19.5
-		pose.pose.pose.position.z = 0.0
-		pose.pose.pose.orientation.z = 0.0
-		pose.pose.pose.orientation.w = 1.0
+			pose.pose.pose.position.x = 17.5
+			pose.pose.pose.position.y = 19.5
+			pose.pose.pose.position.z = 0.0
+			pose.pose.pose.orientation.z = 0.0
+			pose.pose.pose.orientation.w = 1.0
 
-		pub.publish(pose)
+			pub.publish(pose)
 
-		key = 2
+			key = 2
 		
 if __name__ == '__main__': 
 	try:	
